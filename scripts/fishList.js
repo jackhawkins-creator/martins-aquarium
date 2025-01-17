bf2926dd44929cad84180001eb56e4398cc40c2a
Jack Hawkins <jack.m.hawkins@vanderbilt.edu>
2025-01-17 11:41:02 -0600

    Implemented CSS Layout

--- scripts/fishList.js
+++ scripts/fishList.js
@@ -6,20 +6,20 @@ export const fishList = () => {
   let fishObj = database.fish
   for (const fishItem of fishObj) {
     fishThingsHTML += `
-        <article>
-            <img src="${fishItem.image}" alt="${fishItem.name} is a ${fishItem.species}">
-            <div>
-                <ul>
-                    <li>Name: ${fishItem.name}</li>
-                    <li>Species: ${fishItem.species}</li>
-                    <li>Length: ${fishItem.length}</li>
-                    <li>Location: ${fishItem.location}</li>
-                    <li>Diet: ${fishItem.diet}</li>
-                </ul>
-            </div>
-        </article>
+      <article>
+        <img src="${fishItem.image}" alt="${fishItem.name} is a ${fishItem.species}">
+          <div>
+              <ul class="data">
+                  <li>Name: ${fishItem.name}</li>
+                  <li>Species: ${fishItem.species}</li>
+                  <li>Length: ${fishItem.length}</li>
+                  <li>Location: ${fishItem.location}</li>
+                  <li>Diet: ${fishItem.diet}</li>
+              </ul>
+          </div>
+      </article>
     `;
   }
-  
+
   return fishThingsHTML;
 };
